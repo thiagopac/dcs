a7dcb700fb3e3a5b37506f7f0fce84625d8d5981
Roberto Sampaio <roberto.sampaio@ibrowse-sds.com.br>
2023-10-05 17:45:16 +0000

    estrutura inicial do projeto, mapeamento de entidades

---
+++ src/main/java/br/gov/mg/uberlandia/decserver/entity/GenericEntity.java
@@ -0,0 +1,32 @@
+package br.gov.mg.uberlandia.decserver.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@MappedSuperclass
+@JsonIgnoreProperties(ignoreUnknown = true)
+public abstract class GenericEntity {
+
+    @Column(name = "DS_USU_ALTER", nullable = false, length = 30)
+    protected String dsUsuAlter;
+
+    @Column(name = "DT_ULT_ALTER", nullable = false, insertable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    protected Date dtUltAlter;
+
+    @Version
+    @Column(name = "VS_VERSAO", nullable = false, length = 12, insertable = false)
+    protected Long vsVersao;
+
+    @PreUpdate
+    public void preUpdate() {
+        dtUltAlter = new Date();
+        vsVersao = vsVersao == null ? 0L : vsVersao + 1;
+    }
+
+}
---
+++ src/test/java/br/gov/mg/uberlandia/decserver/DecServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.mg.uberlandia.decserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
