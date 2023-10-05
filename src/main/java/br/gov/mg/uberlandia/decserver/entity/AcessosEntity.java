5508ccddfe00327b77bf27b0a7b4cd530eccd852
Pc01 <bruno.contabil.fad@hotmail.com>
2023-10-05 14:27:36 -0300

    estrutura inicial do projeto, mapeamento de entidades

---
+++ src/main/java/br/gov/mg/uberlandia/decserver/entity/AcessosEntity.java
@@ -0,0 +1,48 @@
+package br.gov.mg.uberlandia.decserver.entity;
+
+
+import io.swagger.annotations.ApiModel;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Date;
+
+@ApiModel(description = "TABELA DE ACESSOS.")
+@Data
+@Entity
+@Table(name = "ACESSOS", schema = "DB_DEC")
+public class AcessosEntity extends GenericEntity{
+
+    @Id @SequenceGenerator(name = "DB_DEC.ACESSOS", sequenceName = "DB_DEC.ACESSOS", allocationSize = 1)
+    @GeneratedValue(generator = "DB_DEC.ACESSOS", strategy = GenerationType.AUTO)
+    @Column(name = "OID_ACESSOS", nullable = false, length = 12)
+    private Long oidAcessos;
+
+    @Column(name = "OID_EMPRESA", nullable = false, length = 12)
+    private Long oidEmpresa;
+
+    @Column(name = "NM_ACESSO", nullable = false, length = 100)
+    private String nmAcesso;
+
+    @Column(name = "CPF_CNPJ_ACESSO", nullable = false, length = 15)
+    private String cpfCnpjAcesso;
+
+    @Column(name = "NR_TEL_ACESSO", nullable = false, length = 12)
+    private Long nrTelAcesso;
+
+    @Column(name = "DS_EMAIL_ACESSO", nullable = false, length = 100)
+    private String dsEmailAcesso;
+
+    @Column(name = "STATUS_ACESSO", nullable = false, length = 1)
+    private Long statusAcesso;
+
+    @Column(name = "DS_USU_ALTER", nullable = false, length = 30)
+    private String dsUsuAlter;
+
+    @Column(name = "DT_ULT_ALTER", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dtUltAlter;
+
+    @Column(name = "VS_VERSAO", nullable = false, length = 10)
+    private Long vsVersao;
+
+}
---
+++ src/main/java/br/gov/mg/uberlandia/decserver/entity/EmpresasEntity.java
@@ -0,0 +1,43 @@
+package br.gov.mg.uberlandia.decserver.entity;
+
+import io.swagger.annotations.ApiModel;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Date;
+
+@ApiModel(description = "TABELA DE EMPRESAS.")
+@Data
+@Entity
+@Table(name = "EMPRESAS", schema = "DB_DEC")
+
+public class EmpresasEntity extends GenericEntity{
+
+    @Id @SequenceGenerator(name = "DB_DEC.EMPRESAS", sequenceName = "DB_DEC.EMPRESAS", allocationSize = 1)
+    @GeneratedValue(generator = "DB_DEC.EMPRESAS", strategy = GenerationType.AUTO)
+    @Column(name = "OID_EMPRESAS", nullable = false, length = 12)
+    private Long oidEmpresas;
+
+    @Column(name = "NM_EMPRESA", nullable = false, length = 100)
+    private String nmEmpresa;
+
+    @Column(name = "CNPJ_EMPRESA", nullable = false, length = 100)
+    private String cnpjEmpresa;
+
+    @Column(name = "NR_TEL_EMPRESA", nullable = false, length = 12)
+    private Long nrTelEmpresa;
+
+    @Column(name = "DS_EMAIL_EMPRESA", nullable = false, length = 100)
+    private String dsEmailEmpresa;
+
+    @Column(name = "DS_USU_ALTER", nullable = false, length = 30)
+    private String dsUsuAlter;
+
+    @Column(name = "DT_ULT_ALTER", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dtUltAlter;
+
+    @Column(name = "VS_VERSAO", nullable = false, length = 10)
+    private Long vsVersao;
+
+
+}
