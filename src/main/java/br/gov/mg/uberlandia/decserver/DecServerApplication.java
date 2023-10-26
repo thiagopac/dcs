9df6a33875c36177f06d144a699a2aff2e31267e
Thiago Pires <thiagopac@gmail.com>
2023-10-26 15:19:17 -0300

    Rodando os serviços de executar envios programados e marcar envios como lidos via scheduled tasks

--- src/main/java/br/gov/mg/uberlandia/decserver/DecServerApplication.java
+++ src/main/java/br/gov/mg/uberlandia/decserver/DecServerApplication.java
@@ -1,13 +1,33 @@
 package br.gov.mg.uberlandia.decserver;
 
+import java.util.concurrent.Executor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class DecServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DecServerApplication.class, args);
 	}
 
+@Bean(name = "fileExecutor")
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(30);
+        executor.setMaxPoolSize(30);
+        executor.setKeepAliveSeconds(30);
+        executor.setAllowCoreThreadTimeOut(true);
+        executor.setThreadNamePrefix("Async-");
+        executor.initialize();
+        return executor;
+    }
+	
 }
